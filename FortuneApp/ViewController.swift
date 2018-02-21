5bec608e7eb3b50a56dfbfd1f12e6efacd3fde6e
Bob Kexit <bobkexit@gmail.com>
2018-02-21 19:58:21 +0300

    Initial Commit

---
+++ FortuneApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FortuneApp
+//
+//  Created by Николай Маторин on 21.02.2018.
+//  Copyright © 2018 Николай Маторин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
