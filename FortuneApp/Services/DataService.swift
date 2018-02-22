f767c4001807abaef94be4cf7e4020cae527a616
Bob Kexit <bobkexit@gmail.com>
2018-02-23 00:45:31 +0300

    setup UI

--- FortuneApp/Services/DataService.swift
+++ FortuneApp/Services/DataService.swift
@@ -9,24 +9,32 @@
 import Foundation
 import SwiftyJSON
 
+typealias CompletionHandler = (_ prediction: String?, _ error: Error?) -> ()
+
 class DataService {
     static let instance = DataService()
     
     private init() {}
     
-    func makePrediction() -> String? {
+    func makePrediction(completion: @escaping CompletionHandler) {
         
         guard let url = Bundle.main.url(forResource: "data", withExtension: "json") else {
-            return nil
+            return
         }
         
         do {
             let data = try Data(contentsOf: url)
             let json = try JSON(data: data)
+            
+            let randomIndex = arc4random_uniform(UInt32(json.count))
+            
+            let prediction = json[String(randomIndex)]["ru"].stringValue
+            
+            completion(prediction, nil)
+            
         } catch {
-            debugPrint("Can't parse json file: \(error)")
+            completion(nil, error)
         }
         
-        return ""
     }
 }
