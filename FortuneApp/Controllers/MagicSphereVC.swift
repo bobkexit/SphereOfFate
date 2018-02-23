521020e35e665c941d76686de4c418810a76193c
Bob Kexit <bobkexit@gmail.com>
2018-02-23 12:20:35 +0300

    add Shake ball animation

---
+++ FortuneApp/Controllers/MagicSphereVC.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  FortuneApp
+//
+//  Created by Николай Маторин on 21.02.2018.
+//  Copyright © 2018 Николай Маторин. All rights reserved.
+//
+
+import UIKit
+
+class MagicSphereVC: UIViewController {
+
+    @IBOutlet weak var predictionLbl: UILabel!
+    @IBOutlet weak var ballImgView: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.predictionLbl.text = ""
+        
+        
+    }
+
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if event?.subtype == .motionShake {
+            
+            ballImgView.shake()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                self.updatePredictionLbl()
+            })
+        }
+    }
+    
+    func updatePredictionLbl() {
+        predictionLbl.text = ""
+        DataService.instance.makePrediction { (prediction, error) in
+            if let error = error {
+                self.predictionLbl.text = "Упс, кажется шар сломался..."
+                debugPrint(error)
+            }
+            
+            guard let prediction = prediction else {
+                return
+            }
+            
+            self.predictionLbl.text = prediction
+        }
+    }
+}
+
+
---
+++ FortuneApp/Extensions/UIViewExt.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExt.swift
+//  FortuneApp
+//
+//  Created by Николай Маторин on 23.02.2018.
+//  Copyright © 2018 Николай Маторин. All rights reserved.
+//
+
+import UIKit
+
+class UIViewExt: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
